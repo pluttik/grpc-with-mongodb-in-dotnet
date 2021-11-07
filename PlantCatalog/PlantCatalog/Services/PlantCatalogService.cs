8c22e5767dc60c90784a3bc52ab974ecbc5eaf01
pluttik <drentluttik@gmail.com>
2021-11-07 16:47:42 +0100

    first commit

---
+++ PlantCatalog/PlantCatalog/Services/PlantCatalogService.cs
@@ -0,0 +1,127 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace PlantCatalog.Services
+{
+    public class PlantCatalogService : PlantCatalog.PlantCatalogBase
+    {
+        private readonly ILogger<PlantCatalogService> _logger;
+        private static readonly MongoClient mongoClient = new("mongodb://localhost:27017");
+        private static readonly IMongoDatabase mongoDb = mongoClient.GetDatabase("plantdb");
+        private static readonly IMongoCollection<BsonDocument> mongoCollection = mongoDb.GetCollection<BsonDocument>("plant");
+
+        public PlantCatalogService(ILogger<PlantCatalogService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CreatePlantReply> CreatePlant(CreatePlantRequest request, ServerCallContext context)
+        {
+            var plant = request.Plant;
+            BsonDocument doc = new BsonDocument("common_name", plant.CommonName)
+                .Add("latin_name", plant.LatinName)
+                .Add("description", plant.Description);
+            mongoCollection.InsertOne(doc);
+            string id = doc.GetValue("_id").ToString();
+            plant.Id = id;
+
+            return Task.FromResult(new CreatePlantReply()
+            {
+                Plant = plant,
+            });
+        }
+
+        public override Task<ReadPlantReply> ReadPlant(ReadPlantRequest request, ServerCallContext context)
+        {
+            var plantId = request.Id;
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(plantId));
+            var result = mongoCollection.Find(filter).FirstOrDefault();
+
+            if (result == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Plant with id " + plantId + " was not found."));
+            }
+
+            Plant plant = new()
+            {
+                CommonName = result.GetValue("common_name").AsString,
+                LatinName = result.GetValue("latin_name").AsString,
+                Description = result.GetValue("description").AsString,
+            };
+
+            return Task.FromResult(new ReadPlantReply()
+            {
+                Plant = plant,
+            });
+        }
+
+        public override Task<UpdatePlantReply> UpdatePlant(UpdatePlantRequest request, ServerCallContext context)
+        {
+            var plantId = request.Plant.Id;
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(plantId));
+            var result = mongoCollection.Find(filter).FirstOrDefault();
+
+            if (result == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Plant with id " + plantId + " was not found."));
+            }
+
+            BsonDocument doc = new BsonDocument("common_name", request.Plant.CommonName)
+                .Add("latin_name", request.Plant.LatinName)
+                .Add("description", request.Plant.Description);
+            mongoCollection.ReplaceOne(filter, doc);
+
+            Plant plant = new Plant()
+            {
+                CommonName = doc.GetValue("common_name").AsString,
+                LatinName = doc.GetValue("latin_name").AsString,
+                Description = doc.GetValue("description").AsString,
+            };
+            return Task.FromResult(new UpdatePlantReply()
+            {
+                Plant = plant,
+            });
+        }
+
+        public override Task<DeletePlantReply> DeletePlant(DeletePlantRequest request, ServerCallContext context)
+        {
+            var plantId = request.Id;
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(plantId));
+
+            var result = mongoCollection.DeleteOne(filter);
+
+            if (result.DeletedCount == 0)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Plant with id " + plantId + " was not found."));
+            }
+
+            return Task.FromResult(new DeletePlantReply()
+            {
+                Id = plantId,
+            });
+        }
+
+        public override async Task ListPlant(ListPlantRequest request, IServerStreamWriter<ListPlantReply> responseStream, ServerCallContext context)
+        {
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Empty;
+            var result = mongoCollection.Find(filter);
+
+            foreach (var doc in result.ToList())
+            {
+                await responseStream.WriteAsync(new ListPlantReply()
+                {
+                    Plant = new Plant()
+                    {
+                        Id = doc.GetValue("_id").ToString(),
+                        CommonName = doc.GetValue("common_name").AsString,
+                        LatinName = doc.GetValue("latin_name").AsString,
+                        Description = doc.GetValue("description").AsString,
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ PlantCatalog/PlantClient/Program.cs
@@ -0,0 +1,108 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace PlantClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var plantClient = new PlantCatalog.PlantCatalog.PlantCatalogClient(channel);
+
+            // await CreatePlant(plantClient);
+            // await ReadPlant(plantClient);
+            // await UpdatePlant(plantClient);
+            // await DeletePlant(plantClient);
+            await ListPlant(plantClient);
+            
+            channel.ShutdownAsync().Wait();
+        }
+
+        private static async Task CreatePlant(PlantCatalog.PlantCatalog.PlantCatalogClient plantClient)
+        {
+            var reply = await plantClient.CreatePlantAsync(new PlantCatalog.CreatePlantRequest()
+            {
+                Plant = new PlantCatalog.Plant()
+                {
+                    CommonName = "Baltic pine",
+                    LatinName = "Pinus sylvestris",
+                    Description = "Tree species of the pine family native to Eurasia."
+                }
+            });
+
+            Console.WriteLine("This plant was created: " + reply.Plant);
+        }
+
+        private static async Task ReadPlant(PlantCatalog.PlantCatalog.PlantCatalogClient plantClient)
+        {
+            try
+            {
+                var reply = await plantClient.ReadPlantAsync(new PlantCatalog.ReadPlantRequest()
+                {
+                    // use an existing id from the database here
+                    Id = "61865dfe1c481d4dc22d22c0",
+                });
+
+                Console.WriteLine("This plant was retrieved: " + reply.Plant.ToString());
+            }
+            catch(RpcException rpcException)
+            {
+                Console.WriteLine(rpcException.Status.Detail);
+            }
+        }
+
+        private static async Task UpdatePlant(PlantCatalog.PlantCatalog.PlantCatalogClient plantClient)
+        {
+            try
+            {
+                var reply = await plantClient.UpdatePlantAsync(new PlantCatalog.UpdatePlantRequest()
+                {
+                    Plant = new PlantCatalog.Plant()
+                    {
+                        // use an existing id from the database here
+                        Id = "61865dfe1c481d4dc22d22c0",
+                        CommonName = "Daisy",
+                        LatinName = "Bellis perennis",
+                        Description = "Native European flowering plant."
+                    }
+                });
+
+                Console.WriteLine("This plant was updated: " + reply.Plant.ToString());
+            }
+            catch (RpcException rpcException)
+            {
+                Console.WriteLine(rpcException.Status.Detail);
+            }
+        }
+
+        private static async Task DeletePlant(PlantCatalog.PlantCatalog.PlantCatalogClient plantClient)
+        {
+            try
+            {
+                var reply = await plantClient.DeletePlantAsync(new PlantCatalog.DeletePlantRequest()
+                {
+                    Id = "61864ffe1100cedc3add79c8",
+                });
+
+                Console.WriteLine("Plant with this id was deleted: " + reply.Id.ToString());
+            }
+            catch (RpcException rpcException)
+            {
+                Console.WriteLine(rpcException.Status.Detail);
+            }
+        }
+
+        private static async Task ListPlant(PlantCatalog.PlantCatalog.PlantCatalogClient plantClient)
+        {
+            var reply = plantClient.ListPlant(new PlantCatalog.ListPlantRequest());
+
+            while (await reply.ResponseStream.MoveNext())
+            {
+                Console.WriteLine(reply.ResponseStream.Current.Plant.ToString());
+            }
+        }
+    }
+}
